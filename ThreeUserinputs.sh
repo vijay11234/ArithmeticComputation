82a18d1deeece48b056bcc583ec6656c1c086fe8
vijay11234 <victory6923@gmail.com>
2020-09-26 15:09:52 +0530

    Three inputs from user

---
+++ ThreeUserinputs.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -x
+
+#Taking the Three inputs from the user
+read -p "Enter the first Input " Firstuser
+read -p "Enter the first Input " Seconduser
+read -p "Enter the first Input " Thirduser
+
+echo "Result : $Firstuser $Seconduser $Thirduser"
