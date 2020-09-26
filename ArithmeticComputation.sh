25dd7a71826d2ad791a608f209278813b96bff0c
vijay11234 <victory6923@gmail.com>
2020-09-26 16:01:46 +0530

    Applied the arithmetic

---
+++ ArithmeticComputation.sh
@@ -0,0 +1,20 @@
+  
+#!/bin/bash -x
+
+#Taking the Three inputs from the user
+read -p "Enter the first Input " Firstuser
+read -p "Enter the first Input " Seconduser
+read -p "Enter the first Input " Thirduser
+
+a=$Firstuser
+b=$Seconduser
+c=$Thirduser
+
+result1=$((a+b*c))
+	echo "$a+$b*$c = $result1"
+result2=$((a*b+c))
+	echo "$a*$b+$c = $result2"
+result3=`echo "scale=2; $c+$a/$b" |bc`
+	echo "$c+$a/$b = $result3"
+result4=$((a%b+c))
+	echo "$a%$b+$c = $result4"
